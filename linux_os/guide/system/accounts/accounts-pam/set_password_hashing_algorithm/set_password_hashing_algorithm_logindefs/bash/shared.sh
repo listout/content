94be113594561b3e16d8b5b921c176fb599352c1
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2025-06-10 12:23:13 +0300

    Move login_defs_path to use /usr/etc/login.defs for slmicro6 for set_password_hashing_algorithm_logindefs rule

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/bash/shared.sh
@@ -1,5 +1,11 @@
 # platform = multi_platform_all
 
+{{% if product == 'slmicro6' %}}
+LOGIN_DEFS_PATH=/usr/etc/login.defs
+{{% else %}}
+LOGIN_DEFS_PATH=/etc/login.defs
+{{% endif %}}
+
 {{{ bash_instantiate_variables("var_password_hashing_algorithm") }}}
 
 # Allow multiple algorithms, but choose the first one for remediation
